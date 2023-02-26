53c74885fd160c7cedd3ca191072c6155f925c67
Álvaro <ainiguezdislaa@gmail.com>
2023-02-26 18:18:15 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,8 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(dominio.ej14_7.menornumerolista(lista));
+    }
 }
--- src/dominio/ej14_7.java
+++ src/dominio/ej14_7.java
@@ -1,2 +1,8 @@
-package dominio;public class ej14_7 {
+package dominio;
+import java.util.stream.Stream;
+public class ej14_7 {
+    public static int menornumerolista(int[] lista){
+        return Stream.iterate(0, i -> i + 1).limit(lista.length).map(i -> lista[i]).min(Integer::compare).get();
+
+    }
 }
